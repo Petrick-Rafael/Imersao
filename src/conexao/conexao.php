14bf8f79d95c4fbbc7666b1b69a9b15a33cb6cee
Petrick-Rafael <pekrafael@gmail.com>
2020-04-08 10:09:42 -0300

    Commit - backup

--- src/conexao/conexao.php
+++ src/conexao/conexao.php
@@ -1,6 +1,6 @@
 <?php
 
-    date_default_timezone_set("America/Sao_Paulo");
+    date_default_timezone_set('America/Sao_Paulo');
 
-    $con = mysqli_connect('localhost','root','','db_blog')
+    $conexao = @mysqli_connect('localhost', 'root', '', 'db_blog')
     or die('Erro ao conectar o banco de dados...');
