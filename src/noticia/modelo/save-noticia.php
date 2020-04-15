03467d86ffca53954d6d0b59748e344b8357fdf8
Petrick-Rafael <pekrafael@gmail.com>
2020-04-15 09:55:35 -0300

    Commit - Noticias Finalizado

--- src/noticia/modelo/save-noticia.php
+++ src/noticia/modelo/save-noticia.php
@@ -11,7 +11,8 @@
 
             $idNoticia = isset($requestData['idnoticia']) ? $requestData['idnoticia'] : '';
             $titulo = utf8_decode($requestData['titulo']);
-            $corpo = preg_replace("/(\\r)?\\n/i", "<br/>", $resquestData['corpo']);
+            $corpo = preg_replace("/(\\r)?\\n/i", "<br/>", $requestData['corpo']);
+            $corpo = utf8_decode($corpo);
             $dataNoticia = str_replace('/','-',$requestData['data_noticia']);
             $dataNoticia = date('Y-m-d H:i:s', strtotime($dataNoticia));
             $idCategoria = $requestData['idcategoria'];
